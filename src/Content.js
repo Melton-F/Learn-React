fd6b8b3d3b4c6bcf86cf9f91deb66aec32493683
Melton-F <melton@sparkouttech.com>
2023-09-01 00:11:24 +0530

    use state

--- src/Content.js
+++ src/Content.js
@@ -1,12 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Content = () => {
-
-    const handleWords = ()=>{
-        const words = ["Nallavan", "Arivillaadhavan", "Salli", "Saani", "Item"]
-        const i = Math.floor(Math.random()*5)
-        return words[i]
-      }
 
     const handleClick = (params)=>{
         console.log(params + " Clicked")
@@ -16,14 +10,40 @@ export const Content = () => {
         console.log(event.target.innerText)
     }
 
+    const[count, setCount] = useState(1)
+    const handleIncrement = () =>{
+        setCount(count+1)
+    }
+    const handleDeccrement = () =>{
+        setCount(count-1)
+    }
+
+    const[word, setWord] = useState("New")
+    const handleWords = () =>{
+        const i = Math.floor(Math.random()*5)
+        const words = ["good", "bad", "naughty", "rugged", "innocent"]
+        setWord(words[i])
+    }
+
+
   return (
    <main>
-    <p>Sanjay oru {handleWords()}</p>
+
     <button onClick={() =>handleClick("Melton")}>Click</button> 
     <br />
     <button onClick={(event)=> checkEvent(event)}>check</button>
     <br />
     <button onDoubleClick={() =>handleClick("double")}>Double click</button>
+    <br />
+
+    <button onClick={handleDeccrement}>-</button>
+    <span>{count}</span>
+    <button onClick={handleIncrement}>+</button>
+    <br />
+
+    <p>I'm a {word} boy</p>
+    <button onClick={handleWords}>subscribe</button>
+    
    </main>
   )
 }
